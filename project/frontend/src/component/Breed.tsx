57f1b802f4a37090fd27931c5fdcffcbdddee116
HyunmoBae <tsi0520@naver.com>
2022-07-08 15:50:25 +0900

    fetch()함수

--- project/frontend/src/component/Breed.tsx
+++ project/frontend/src/component/Breed.tsx
@@ -1,12 +1,32 @@
-import axios from "axios";
-import useSWR from "swr";
+// import axios from "axios";
+// import useSWR from "swr";
 
-export default function Container() {
-  const { data: abc } = useSWR(`http://localhost:3001/breeds`, () => fetcher(`http://localhost:3001/breeds`));
-  const fetcher = (url: string) => axios.get(url);
-  console.log(abc?.data[1].name); //axios 응답 -> json 형태
+// export default function Container() {
+//   const { data: abc } = useSWR(`http://localhost:3001/breeds`, () => fetcher(`http://localhost:3001/breeds`));
+//   const fetcher = (url: string) => axios.get(url);
+//   console.log(abc?.data[1].name); //axios 응답 -> json 형태
 
-  const getName = abc?.data[1].name;
+//   const getName = abc?.data[1].name;
 
-  return <h1>{getName}</h1>;
-}
+//   return <h1>{getName}</h1>;
+// }
+
+import { useEffect, useState } from "react";
+
+export default function Day() {
+    const [breeds, setbreed]:any = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/breeds/Beagle')
+        .then(res=> {
+            return res.json();
+        })
+        .then(data => {
+            setbreed(data);
+        });
+    }, []);
+
+    return (
+      <h1>{breeds.name}</h1>
+      )
+  }
